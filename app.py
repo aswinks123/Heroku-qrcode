c6457081d3c305f171c75e8fe9aeed08016801df
Aswinks1995 <aswinks1995@outlook.com>
2022-08-14 20:18:35 +0530

    Added button styling

--- app.py
+++ app.py
@@ -39,18 +39,18 @@ def QR():
 
     # Adding Progress bar
     put_processbar('bar')
-    for i in range(1, 4):
-        set_processbar('bar', i / 3)
-        time.sleep(0.1)
+    for i in range(1, 3):
+        set_processbar('bar', i / 2)
+        time.sleep(0.2)
 
     #getting foreground color
     foreground = select("Choose Foreground Color", options=['Black', 'Red', 'Blue', 'White','Yellow','Cyan','Magenta'], required=True)
 
     # Adding Progress bar
     put_processbar('bar')
-    for i in range(1, 4):
-        set_processbar('bar', i / 3)
-        time.sleep(0.1)
+    for i in range(1, 3):
+        set_processbar('bar', i / 2)
+        time.sleep(0.2)
 
     #getting background color
     background = select("Choose Background Color", options=['White', 'Red', 'Black', 'Blue','Yellow','Cyan','Magenta'], required=True)
@@ -69,7 +69,8 @@ def QR():
         time.sleep(0.1)
 
     #outputing the text and image
-    put_text("Your QR Code is Created.")
+    #put_text("Your QR Code is Created.")
+    put_html(r"""<h3  align="center"><strong>Your QR Code is Created</strong></h3>""")
     img = open('qr-image.jpg', 'rb').read()
     put_image(img, width='150px')
 
@@ -77,44 +78,48 @@ def QR():
     put_file('qr-image.jpg', content, 'Download QR code')
 
 
-    #To show About session
+    #To show About sessiono
     def clicked():
         popup('About Us', [
             put_html('<h2>Created by Aswin Ks</h2>'),
-            put_html('<h3>This Project uses Python, Pywebio and Flask</h3>'),
+            put_html('<h3>This Project is created using Python, Pywebio and Flask</h3>'),
             'Find More @ https://github.com/aswinks1995',  # equal to put_text('plain html: <br/>')
-
             put_buttons(['close'], onclick=lambda _: close_popup())
         ])
 
     global like
     like = 0
 
-    #To register the like count(in beta stage).Thsi feature is yet to be implemented.
+    #To register the like count(in beta stage)
     def liked():
         global like
         like += 1
         put_info("Thanks For your Feedback!")
 
 
-    #Button to go to main screen
-    put_button("Generate new QR Code", onclick=lambda: run_js('window.location.reload()'))
-    #About us Button
-    put_button("About Us", onclick=lambda: clicked())  # single button
-    #Like Button
-    put_button("Like", onclick=lambda: liked())
+
+    def btn_click(btn_val): #To do function of the 3 buttons
+
+        if btn_val=='About':  #btn_val contain the text of button
+            clicked()
+        elif btn_val=='Generate new QR Code':
+            run_js('window.location.reload()')
+        elif btn_val=='Like':
+            liked()
+
+
+    put_buttons(['Generate new QR Code', 'About', 'Like'], onclick=btn_click)#Buttons
 
 
 #To allow reloading of web browser and mentioning the port
 app.add_url_rule('/qr','webio_view',webio_view(QR),methods=['GET','POST','OPTIONS'])
-
-
-#To Start the Server
+# app.run(port=44556 ,debug=True)
 if __name__ =='__main__':
     parser=argparse.ArgumentParser()
     parser.add_argument("-p","--port",type=int,default=8080)
     args=parser.parse_args()
 
-    start_server(QR,port=args.port)
-
+    start_server(QR,port=args.port,debug=True)
+#calling our main function.Program start here
+# QR()
 
