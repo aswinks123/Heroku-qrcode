50b1e4f5e59f54d2562e84f580f17afe8d209849
Aswinks1995 <aswinks1995@outlook.com>
2022-08-14 19:40:54 +0530

    added

--- app.py
+++ app.py
@@ -1,78 +1,120 @@
-#Created By : Aswin KS
-#This API check whether a number is plindrome or amstrong
-
-from flask import Flask, request, render_template,jsonify
-
-app = Flask(__name__)
-
-@app.route('/')
-def hello_world():
-    print("not Amstrong")
+#Created By Aswin KS
+#This project uses Python,Pywebio,Flask and other modules to Generate QR codes.
+#Feel free to clone the code.
 
 
-@app.route('/amstrong/<int:n>')
-def amstrong(n):
-    sum=0
-    order=len(str(n))
-    copy_n=n
-    while(n>0):
-        digit=n%10
-        sum+=digit **order
-        n=n//10
-    if (sum==copy_n):
+#Importing Modules
+import argparse
 
-        result={
+from pywebio.input import *
+from pywebio.output import *
+from flask import  Flask
+from pywebio.session import *
+from pywebio.platform.flask import webio_view
+from pywebio import STATIC_PATH
+import qrcode
+from PIL import Image
+import re
+import time
+from pywebio.session import run_js
+from pywebio import start_server
+#-----------------------------------------------------------------------------------------------------------------------
 
-            "Number":copy_n,
-            "Is Amstrong ?": True
+#Creating  a flask app
 
-        }
+app=Flask(__name__)
 
-
-    else:
-        print("not Amstrong")
-        result = {
-
-            "Number": copy_n,
-            "Amstrong": False
-
-        }
-
-    return jsonify(result)
-
-@app.route('/palindrome/<int:n>')
-def palindrome(n):
-    x=n
-    num = n
-    temp = num
-    rev = 0
-    while(num > 0):
-        dig = num % 10
-        rev = rev * 10 + dig
-        num = num // 10
-    if(temp == rev):
-        result = {
-
-            "Number": x,
-            "Palindrome": True
-
-        }
-    else:
-        result = {
-
-            "Number": x,
-            "Palindrome": False
-
-        }
-    return jsonify(result)
+#Main function that creates the QR code
+def QR():
+    c=0 #for like count
 
 
+    set_env(title="QR-Code Generator") #For setting the window title bar text(tab text)
+
+    put_html(r"""<h1  align="center"><strong>📱Color QR-Code Generator</strong></h1>""") #App Name in Main screen
+    img = open('logo.png', 'rb').read()  #logo
+    put_image(img, width='100px')#size of image
 
 
+    x=input("Enter the URL or Text to Create QR-code",type="text",required=True)#Accepting url or data
+
+    # Adding Progress bar
+    put_processbar('bar')
+    for i in range(1, 3):
+        set_processbar('bar', i / 2)
+        time.sleep(0.1)
+
+    #getting foreground color
+    foreground = select("Choose Foreground Color", options=['Black', 'Red', 'Blue', 'White','Yellow','Cyan','Magenta'], required=True)
+
+    # Adding Progress bar
+    put_processbar('bar')
+    for i in range(1, 3):
+        set_processbar('bar', i / 2)
+        time.sleep(0.1)
+
+    #getting background color
+    background = select("Choose Background Color", options=['White', 'Red', 'Black', 'Blue','Yellow','Cyan','Magenta'], required=True)
+
+    #Creating QR code using the provided data
+    feature = qrcode.QRCode(version=1, box_size=40, border=3)
+    feature.add_data(x)
+    feature.make(fit=True)
+    img = feature.make_image(fill_color=foreground, back_color=background)
+    img.save("qr-image.jpg")
+
+    #Adding Progress bar
+    put_processbar('bar')
+    for i in range(1, 8):
+        set_processbar('bar', i /7)
+        time.sleep(0.1)
+
+    #outputing the text and image
+    put_text("Your QR Code is Created.")
+    img = open('qr-image.jpg', 'rb').read()
+    put_image(img, width='150px')
+
+    content = open('qr-image.jpg', 'rb').read()
+    put_file('qr-image.jpg', content, 'Download QR code')
 
 
+    #To show About sessiono
+    def clicked():
+        popup('About Us', [
+            put_html('<h2>Created by Aswin Ks</h2>'),
+            put_html('<h3>This Project uses Python, Pywebio and Flask</h3>'),
+            'Find More @ https://github.com/aswinks1995',  # equal to put_text('plain html: <br/>')
+
+            put_buttons(['close'], onclick=lambda _: close_popup())
+        ])
+
+    global like
+    like = 0
+
+    #To register the like count(in beta stage)
+    def liked():
+        global like
+        like += 1
+        put_info("Thanks For your Feedback!")
 
 
+    #Button to go to main screen
+    put_button("Generate new QR Code", onclick=lambda: run_js('window.location.reload()'))
+    #About us Button
+    put_button("About Us", onclick=lambda: clicked())  # single button
+    #Like Button
+    put_button("Like", onclick=lambda: liked())
 
-if __name__ == '__main__':
-    app.run(debug=True)
+
+#To allow reloading of web browser and mentioning the port
+app.add_url_rule('/','webio_view',webio_view(QR),methods=['GET','POST','OPTIONS'])
+app.run(port=44556 ,debug=True)
+if __name__ =='__main__':
+    parser=argparse.ArgumentParser()
+    parser.add_argument("-p","--port",type=int,default=8080)
+    args=parser.parse_args()
+
+    start_server(QR,port=args.port)
+#calling our main function.Program start here
+QR()
+
